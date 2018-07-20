6d2f1dd3c265cac6e5ecbe113b597dd4a158fc4b
dell <hpp_hxx@163.com>
2018-07-20 17:40:07 +0800

    api

--- src/App.js
+++ src/App.js
@@ -4,96 +4,16 @@ import './App.css';
 
 let ReactHighcharts = require('react-highcharts');
 let Highcharts = require('highcharts');
-
-const dataSource = [{
-    key: '1',
-    attribute: 'MYCT',
-    type: '数值型(INT)',
-    missValue: '0',
-    distribution: {
-        xAxis: {
-            categories: ['', '17.00', '165.30', '313.60', '461.90', '610.20', '758.50', '906.80']
-        },
-        series: [
-            {
-                name: 'yyy0',
-                data: [0, 140, 30, 15, 2, 1, 10, 0],
-                color: '#639d63',
-            }
-        ],
-    },
-    mixOrLeast: '17',
-    maxOrMost: '1500',
-    standardDeviation: '260.2629'
-}, {
-    key: '2',
-    attribute: 'MMIN',
-    type: '数值型(INT)',
-    missValue: '0',
-    distribution: {
-        xAxis: {
-            categories: ['', '6.4e+1', '3.3e+3', '5.5e+3', '9.6e+3', '0.6e+4', '3.2e+4']
-        },
-        series: [{
-            name: 'yyy1',
-            data: [0, 140, 30, 15, 2, 0, 0],
-            color: '#639d63',
-        }]
-    },
-    mixOrLeast: '64',
-    maxOrMost: '32000',
-    standardDeviation: '3878.7428'
-}];
-
-const columns = [
-    {
-        title: '属性',
-        dataIndex: 'attribute',
-        key: 'attribute',
-    },
-    {
-        title: '类型',
-        dataIndex: 'type',
-        key: 'type',
-    },
-    {
-        title: '缺失值个数',
-        dataIndex: 'missValue',
-        key: 'missValue',
-    },
-    {
-        title: '分布图',
-        dataIndex: 'distribution',
-        key: 'distribution',
-        render: (text, record) => {
-            console.log('text', text);
-            console.log('record', record);
-            return (
-                <div>
-                    <ReactHighcharts config={record.distribution}>
-                    </ReactHighcharts>
-                </div>
-            )
-        }
-    },
-    {
-        title: '最小/最少',
-        dataIndex: 'mixOrLeast',
-        key: 'mixOrLeast',
-    }, {
-        title: '最大/最多',
-        dataIndex: 'maxOrMost',
-        key: 'maxOrMost',
-    },
-    {
-        title: '标准差/数量占比',
-        dataIndex: 'standardDeviation',
-        key: 'standardDeviation',
-    }
-];
-
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            dataSource:[]
+        };
+        this.getDataSource = this.getDataSource.bind(this);
+    }
     componentWillMount() {
+        this.getDataSource();
         Highcharts.theme = {
             chart: {
                 type: 'column',
@@ -130,11 +50,84 @@ class App extends Component {
         };
         Highcharts.setOptions(Highcharts.theme);
     }
+    getDataSource(){
+        fetch(`http://192.168.10.34:3000/statsdata`)
+            .then((res) => (res.json()))
+            .then((res) => {
+                console.log('res',res);
+                this.setState({
+                    dataSource: res
+                });
+            });
 
+    }
     render() {
+        const dataSource = this.state.dataSource;
+        dataSource.map((value,arr)=>{
+            let node = value.distribution.node;
+            let val = value.distribution.value;
+            node = ['',...node];
+            val = [0,...val,0];
+            value.distribution.series = [{
+                name:value.propertyName,
+                data:val,
+                color:'#639d63'
+            }];
+            delete value.distribution.value;
+            value.distribution.xAxis = {
+                categories:node
+            };
+            delete value.distribution.node;
+            return arr;
+        });
+        const columns = [
+            {
+                title: '属性',
+                dataIndex: 'propertyName',
+                key: 'propertyName',
+            },
+            {
+                title: '类型',
+                dataIndex: 'type',
+                key: 'type',
+            },
+            {
+                title: '缺失值个数',
+                dataIndex: 'lossCount',
+                key: 'lossCount',
+            },
+            {
+                title: '分布图',
+                dataIndex: 'distribution',
+                key: 'distribution',
+                render: (text, record) => {
+                    console.log('record-----',record);
+                    return (
+                        <div>
+                            <ReactHighcharts config={record.distribution}>
+                            </ReactHighcharts>
+                        </div>
+                    )
+                }
+            },
+            {
+                title: '最小/最少',
+                dataIndex: 'min',
+                key: 'min',
+            }, {
+                title: '最大/最多',
+                dataIndex: 'max',
+                key: 'max',
+            },
+            {
+                title: '标准差/数量占比',
+                dataIndex: 'standardValue',
+                key: 'standardValue',
+            }
+        ];
         return (
             <div className="App">
-                <Table dataSource={dataSource} columns={columns}/>
+                <Table dataSource={dataSource} columns={columns}  rowKey={record => record.propertyName}/>
             </div>
         );
     }
